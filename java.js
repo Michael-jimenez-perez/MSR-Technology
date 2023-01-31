a58b16f5ab48a8d11f78ea6167e85b507dd73afc
Michael-jimenez-perez <asi3.michael.jimenez@gmail.com>
2023-01-31 13:28:47 +0100

    a

---
+++ java.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    document.getElementById("seccion","").style.opacity = 1;
+  };
+
+window.onload = function() {
+    document.getElementById("header").style.opacity = 1;
+};
