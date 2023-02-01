816629303ef68f868c5ebed82bf3704cc3202fe1
Michael-jimenez-perez <asi3.michael.jimenez@gmail.com>
2023-02-01 10:56:24 +0100

    a

---
+++ prueba1/registra.php
@@ -0,0 +1,35 @@
+<html>   
+
+<head>   
+    <title>Guardamos los datos en la base de datos</title>   
+</head>   
+
+<body>   
+<?php   
+
+// Recibimos por POST los datos procedentes del formulario   
+
+$nombre = $_POST["nombre"];   
+$email = $_POST["email"];   
+$fecha = date("d-m-Y");   
+
+// Abrimos la conexion a la base de datos   
+include("abre_conexion.php");   
+
+$_GRABAR_SQL = "INSERT INTO $tabla_db1 (nombre,email,fecha) VALUES ('$nombre','$email','$fecha')";   
+mysqli_query($conexion_db,$_GRABAR_SQL);  
+
+// Cerramos la conexion a la base de datos   
+include("cierra_conexion.php");   
+
+// Confirmamos que el registro ha sido insertado con exito   
+
+echo "   
+<p>Los datos han sido guardados con exito.</p>   
+
+<p><a href='javascript:history.go(-1)'>VOLVER ATRÁS</a></p>   
+";   
+?>   
+</body>   
+
+</html>  
