d9a16ba32223f4b7f4cb29557bc3ce166ada8d2c
Michael-jimenez-perez <asi3.michael.jimenez@gmail.com>
2023-02-12 15:44:04 +0100

    a

--- donar.js
+++ donar.js
@@ -19,16 +19,3 @@ const goToTop = (){
       });
   };
 backToTopButton.addEventListener("click", goToTop);
-
-var slider = document.getElementById("slider");
-var secciones = slider.getElementsByClassName("seccion2");
-var index = 0;
-
-function mostrarSiguiente() {
-  secciones[index].style.display = "none";
-  index = (index + 1) % secciones.length;
-  secciones[index].style.display = "block";
-}
-
-setInterval(mostrarSiguiente, 3000);
-
