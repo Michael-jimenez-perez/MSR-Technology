f9670712624ab54b9b1741cdfa4184f507214ec4
Michael-jimenez-perez <asi3.michael.jimenez@gmail.com>
2023-02-14 13:09:00 +0100

    a

--- PHP/registrar.php
+++ PHP/registrar.php
@@ -6,7 +6,7 @@ if (isset($_POST['register'])) {
 	    $nombre = trim($_POST['nombre']);
 	    $email = trim($_POST['email']);
 	    $contraseña = trim($_POST['contraseña']);
-	    $consulta = "INSERT INTO clientes(nombre, email, contraseña) VALUES ('$nombre','$email','$contraseña')";
+	    $consulta = "INSERT INTO usuarios(nombre, email, contraseña) VALUES ('$nombre','$email','$contraseña')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
 			header("Location: http://msrtechnology.mywebcommunity.org/index_sesion.html");
